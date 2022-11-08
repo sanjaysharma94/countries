1ae76401be22f0b3dcbd55f502c2c06c62037c54
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-11-08 16:52:12 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,36 @@ import useFetch from "./Fetch"
 
 export const Home = () =>{
 
-    const data= useFetch("https://restcountries.com/v3/all");
-    console.log("🚀 ~ file: Home.jsx ~ line 14 ~ Home ~ ̥",data.data)
-    return(
-        <div></div>
+    const {data,loading}= useFetch("https://restcountries.com/v3/all");
+    console.log(loading)
+    
+    return(<div id="container">
+
+        <div id="search-filter">
+        <input type="text" /> 
+        <select>
+            <option value="">Filter by Region</option>
+            <option value="Asia">Asia</option>
+            <option value="Africa">Africa</option>
+            <option value="Oceania">Oceania</option>
+            <option value="Americas">Americas</option>
+            <option value="Europe">Europe</option>
+
+        </select>
+        </div>
+        <div id="main-div">
+            {data.map((el)=><div className="per-Div" >
+                    <img className="perImage" src={el.flags[1]} alt="country" />
+                    <h4  className="last-Div1">{el.name.common}</h4>
+                    <div className="last-Div">
+                    <p className="highlight" >Population : </p><span>{el.population}</span> <br />
+                    <p className="highlight">Region : </p><span>{el.region}</span> <br />
+                    <p className="highlight">Capital : </p><span>{el.capital}</span>
+                    </div>
+                </div>
+            )}
+        </div>
+        </div>
     )
 }
  
