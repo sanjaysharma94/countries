ad3c6b25862f219b0c598495125dc13efda35d4d
Sanjay Sharma <96072511+sanjaysharma94@users.noreply.github.com>
2022-11-11 15:38:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
 import './App.css';
 import { Home } from './components/Home';
+import { Details } from './components/Elements';
+import {Routes, Route } from "react-router-dom"
 
 function App() {
-  return (
+  return ( <>
+    <div id='Navbar'> <em>Where in the world ?</em> <em>Dark Mode</em> </div>
     <div className="App">
-      <Home/>
+      
+
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route  path='/details/:name' element={<Details/>} />
+      </Routes>
     </div>
+  
+  </>
   );
 }
 
